319e7f03a0cd8b3852d9aafefecb69072f12f5a9
smok <lavit.william@gmail.com>
2019-09-30 14:44:51 +0200

    first commit

---
+++ src/Controller/homeController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Controller;
+use App\Entity\Article;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+class homeController extends AbstractController{
+
+    /**
+     * @Route("/home", name="home")
+     */
+
+    public function home(){
+        return $this->render('index.html.twig');
+    }
+
+     /**
+      * @Route("/articles", name="articles")
+      */
+
+      public function liste_article(EntityManagerInterface $manager){
+
+        $articles = $manager->getRepository(Article::class)->findAll();
+
+        return $this->render("articles.html.twig", array("art" => $articles));
+
+    }
+
+/**
+ * @param EntityManagerInterface $manager
+ * @param Request $request
+ * @Route("/create_new_article", methods="POST", name="create")
+ */
+
+    public function new_article(EntityManagerInterface $manager, Request $request){
+
+        $article = new Article();
+
+        $article->setLibelle($request->get('libelle'))
+        ->setPu($request->get('pu'));
+
+        $manager->persist($article);
+        $manager->flush();
+
+        return $this->redirectToRoute("articles");
+
+    }
+
+      /**
+      * @Route("/personnes", name="personnes")
+      */
+
+      public function personnes(){
+        return $this->render('personnes.html.twig');
+    }
+
+}
+
+?>
---
+++ src/Entity/Article.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ArticleRepository")
+ */
+class Article
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=20)
+     */
+    private $libelle;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $pu;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLibelle(): ?string
+    {
+        return $this->libelle;
+    }
+
+    public function setLibelle(string $libelle): self
+    {
+        $this->libelle = $libelle;
+
+        return $this;
+    }
+
+    public function getPu(): ?int
+    {
+        return $this->pu;
+    }
+
+    public function setPu(int $pu): self
+    {
+        $this->pu = $pu;
+
+        return $this;
+    }
+}
