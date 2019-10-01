d0cd5f010d005e589d592636d227dc0892a56a91
smok <lavit.william@gmail.com>
2019-10-01 09:32:11 +0200

    affichage et insertion des membres

--- src/Controller/homeController.php
+++ src/Controller/homeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 use App\Entity\Article;
+use App\Entity\Personnes;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -62,9 +63,41 @@ $this->addFlush("warning",
       * @Route("/personnes", name="personnes")
       */
 
-      public function personnes(){
-        return $this->render('personnes.html.twig');
+    public function liste_personne(EntityManagerInterface $manager2){
+
+        $personnes = $manager2->getRepository(Personnes::class)->findAll();
+
+        return $this->render("personnes.html.twig", array("pers" => $personnes));
+
     }
+
+    /**
+ * @param EntityManagerInterface $manager
+ * @param Request $request
+ * @Route("/create_new_personnes", methods="POST", name="createPersonne")
+ */
+
+public function new_personnes(EntityManagerInterface $manager, Request $request){
+
+    $personnes = new Personnes();
+
+    $personnes->setNom($request->get('nom'))
+    ->setPrenom($request->get('prenom'));
+
+try{
+$manager->persist($personnes);
+$manager->flush();
+
+}
+catch(UniqueConstraintViolationException $e){
+$this->addFlush("warning", 
+            "Erreur de saisie"
+);
+}
+
+    return $this->redirectToRoute("personnes");
+
+}
 
 }
 
